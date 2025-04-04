21eb9a2eac0c2fb97e4a59475aa47f1d2a87af56
IzharuddinMalik <izharuddin1997@gmail.com>
2025-04-04 13:57:08 +0700

    comment useunifiedtopology

--- server.js
+++ server.js
@@ -14,7 +14,7 @@ app.use(cors());
 // Koneksi ke MongoDB
 mongoose.connect(process.env.MONGO_URI, {
   useNewUrlParser: true,
-  useUnifiedTopology: true,
+  // useUnifiedTopology: true,
 })
 .then(() => console.log("MongoDB Connected"))
 .catch(err => console.log(err));
