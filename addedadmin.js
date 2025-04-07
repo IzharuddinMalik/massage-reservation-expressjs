18c57098b7ef502a830365a7d50e66bf655059fd
IzharuddinMalik <izharuddin1997@gmail.com>
2025-04-07 13:58:56 +0700

    addedadmin.js updated

--- addedadmin.js
+++ addedadmin.js
@@ -3,7 +3,7 @@ const bcrypt = require("bcryptjs");
 const Admin = require("./models/Admin");
 require("dotenv").config();
 
-mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true })
+mongoose.connect(process.env.MONGO_URI)
   .then(async () => {
     const hashedPassword = await bcrypt.hash("admin123", 10);
     const admin = new Admin({ username: "admin", password: hashedPassword });
